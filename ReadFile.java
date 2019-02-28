64d7210b83d878b6c7f2ea52f3a76a57f2c640e0
Amy Fouzia <afouzia00@stuy.edu>
2019-02-28 14:51:25 -0500

    end of class

--- ReadFile.java
+++ ReadFile.java
@@ -20,17 +20,17 @@ public class ReadFile {
 
   }
 
-/* public ReadFile() throws FileNotFoundException{
+  public ReadFile() throws FileNotFoundException{
     File maze1 = new File("Maze1.txt");
     Scanner inf = new Scanner(maze1);
 
-    for (i = 0; i < ; i++){
-      for (j = 0; j < ; j++){
-
+    for (i = 0; i < numLines; i++){
+      for (j = 0; j < numLines; j++){
+        field[i]
       }
     }
 
-  }*/
+  }
 
 
 }
